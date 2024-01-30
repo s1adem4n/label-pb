420586c84188b97768f3ad48f38d6af304b184de
Jonathan <jonathanbuko@icloud.com>
2024-01-30 20:47:42 +0100

    Update release.sh script to handle tag creation and release

--- release.sh
+++ release.sh
@@ -1,6 +1,38 @@
-make -j4 all
-TAG=$(git describe --tags --abbrev=0)
+TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+if [[ -z "$TAG" ]]; then
+    echo "No tag found. Enter a tag:"
+    read TAG
+    git tag $TAG
+else 
+    echo "Tag found: $TAG"
+    echo "Do you want to release this tag? (y/n)"
+    read ANSWER
+    if [[ $ANSWER =~ ^[Yy]$ ]]; then
+        echo "Releasing tag $TAG"
+    else
+        echo "Enter a tag:"
+        read TAG
+        git tag $TAG
+    fi
+fi
+
+# check if tag is already released
+if [[ $(gh release list | grep $TAG) ]]; then
+  echo "Tag $TAG already released"
+  exit 1
+fi
+
+# check if tag is present in origin
+if [[ ! $(git ls-remote --tags origin | grep $TAG) ]]; then
+  git push --tags
+fi
+
 echo "Tag: $TAG"
 
-zip -r releases/${TAG}.zip build/
-gh release create $TAG releases/${TAG}.zip -t $TAG
+# check if zip does not exist
+if [[ ! -f releases/${TAG}.zip ]] || [[ $@ =~ "-f" ]]; then
+  make -j4 --always-make all
+  zip -r releases/${TAG}.zip build/
+fi
+
+gh release create $TAG releases/${TAG}.zip
